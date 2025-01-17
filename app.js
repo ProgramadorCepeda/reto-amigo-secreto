a4c605ceaa07e323553ff0155cf971554085ef70
Juan David Cepeda <programadorcepeda@gmail.com>
2025-01-17 12:56:54 -0500

    Creo la lista y se empieza las funciones

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayAmigos = [];
+let resultado = document.getElementById("resultado");
+function textosIniciales(){
+
+}
+
+function mostrarLista(arrayAmigos){
+    let listaAmigos = document.getElementById("listaAmigos").value;
+    resultado.innerHTML(listaAmigos);
+}
+function agregarAmigo(){
+    let amigo = document.getElementById("amigo").value;
+    arrayAmigos.push(amigo);
+    console.log(arrayAmigos);
+    mostrarLista(arrayAmigos);
+}
+console.log(arrayAmigos)
