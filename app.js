a73b8cb70aad9ab3fac84543b455535c144f5d46
Juan David Cepeda <programadorcepeda@gmail.com>
2025-01-17 13:32:59 -0500

    Pruebas y logica del boton de sorteo

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 let arrayAmigos = [];
 let resultado = document.getElementById("resultado");
+let listaDesplegable = document.getElementById("listaAmigos");
+
 function textosIniciales(){
 
 }
 
-function mostrarLista(arrayAmigos){
-    let listaAmigos = document.getElementById("listaAmigos").value;
-    resultado.innerHTML(listaAmigos);
+function sortearAmigo(){ //Esta funcion hace el sorteo de los amigos
+    let seleccionado = arrayAmigos[Math.floor(Math.random()*arrayAmigos.length)];
+    let prueba = document.getElementById("sorteo");
+    prueba.innerHTML = seleccionado;
+    
+    
 }
-function agregarAmigo(){
+
+function mostrarLista(arrayAmigos){ //Esta funcion muestra la lista del array actualizando el texto
+    
+
+    
+}
+
+function agregarAmigo(){  //Esta funcion añade al amigo al array
     let amigo = document.getElementById("amigo").value;
-    arrayAmigos.push(amigo);
-    console.log(arrayAmigos);
+    if (amigo == ''){
+        alert('Digite un nombre valido por favor');
+    } else{
+        arrayAmigos.push(amigo);
+        amigo.innerHTML = "";
+        console.log(arrayAmigos);
+        // let li = document.createElement("li"); //Creamos un elemento tipo lista
+        // li = arrayAmigos[arrayAmigos.length-1];
+        // listaDesplegable.appendChild(li);
+    }
     mostrarLista(arrayAmigos);
 }
-console.log(arrayAmigos)
